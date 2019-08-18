6b37683e4cb3f85dc12bb6496ab66cd4397f9213
levy <897895407@qq.com>
2019-08-18 12:08:05 +0800

    refactor: 使用函数，精简代码

--- index.js
+++ index.js
@@ -1,41 +1,23 @@
 const titleRegex = /(\w*)(?:\((\w*)\))?: (.*)/
-const defaultConfig = require('./default-config')
+const {getType} = require('./utils')
 
 module.exports = app => {
   app.on(['pull_request.opened', 'pull_request.edited'], async context => {
     app.log('------ receiving webhook ------')
 
-    const config = await context.config(`badge.yml`, defaultConfig)
-    const {owner, repo} = context.repo()
     const {
       payload: {
         pull_request: {title, labels}
       }
     } = context
 
-    if (titleRegex.test(title)) {
-      const [, type, scope] = titleRegex.exec(title)
-      let label = null
-      if (config && config.types[type]) {
-        if (typeof config.types[type] === `string`) {
-          label = config.types[type]
-        } else if (typeof config.types[type] === `object`) {
-          if (scope && config.types[type][scope]) {
-            label = config.types[type][scope]
-          } else if (config.types[type].default) {
-            label = config.types[type].default
-          }
-        }
-      } else if (config.default) {
-        label = config.default
-      }
-      if (label !== null) {
-        context.log(`${owner}/${repo}`, `Using label:`, label)
-        const oldLabels = labels.map(lab => lab.name)
-        const newLabels = Array.from(new Set([...oldLabels, label]))
-        const params = context.issue({labels: newLabels})
-        context.github.issues.addLabels(params)
-      }
-    }
+    let label = getType(title)
+
+    const {owner, repo} = context.repo()
+    context.log(`${owner}/${repo}`, `Using label:`, label)
+    const oldLabels = labels.map(lab => lab.name)
+    const newLabels = Array.from(new Set([...oldLabels, label]))
+    const params = context.issue({labels: newLabels})
+    context.github.issues.addLabels(params)
   })
 }
